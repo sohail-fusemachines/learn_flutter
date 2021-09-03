cb6681ea5326f2dc9056a22a92b200fcea540d30
Sohail Shrestha <xyz@xyz.com>
2021-09-03 14:31:37 +0545

    _

--- lib/di/modules/shared_pref_module.dart
+++ lib/di/modules/shared_pref_module.dart
@@ -5,8 +5,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class SharedPreferencesModule{
-
-
   @preResolve
   @singleton
   Future<SharedPreferences> getSharedPreferences() async => await SharedPreferences.getInstance();
